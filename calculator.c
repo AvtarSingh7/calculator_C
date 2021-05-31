c20d9bbfd7a26fccacb98d2fbb5b2649a04585c6
AvtarSingh7 <85128370+AvtarSingh7@users.noreply.github.com>
2021-06-01 04:08:56 +0530

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -16,7 +16,7 @@ main ()
   printf ("The value of b is %.2f.\n", b);
   printf ("The addition of %.2f and %.2f is %.2f.\n", a, b, a + b);
   printf ("The subtraction of %.2f and %.2f is %.2f.\n", a, b, a - b);
-  printf ("The multiplication of %f and %.2f is %.2f.\n", a, b, a * b);
+  printf ("The multiplication of %.2f and %.2f is %.2f.\n", a, b, a * b);
   printf ("The division of %.2f and %.2f is %.2f.", a, b, a / b);
   return 0;
 }
