11f696d02b8e5f9e615b19593a8707aa994f1d28
AvtarSingh7 <85128370+AvtarSingh7@users.noreply.github.com>
2021-06-01 22:35:01 +0530

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -1,22 +1,20 @@
-
 #include <stdio.h>
 
 int
 main ()
 {
-  float a  ;
-  float b ;
-  
-  printf("Enter an Integer = ");
-  scanf("%f", &a);
-  printf("Enter an Integer  = ");
-  scanf("%f", &b);
+  float a;
+  float b;
+  printf ("Enter an Integer = ");
+  scanf ("%f", &a);
+  printf ("Enter an Integer  = ");
+  scanf ("%f", &b);
   printf ("The two integers are a and b.\n");
   printf ("The value of a is %.2f.\n", a);
   printf ("The value of b is %.2f.\n", b);
-  printf ("The addition of %.2f and %.2f is %.2f.\n", a, b, a + b);
-  printf ("The subtraction of %.2f and %.2f is %.2f.\n", a, b, a - b);
-  printf ("The multiplication of %.2f and %.2f is %.2f.\n", a, b, a * b);
-  printf ("The division of %.2f and %.2f is %.2f.", a, b, a / b);
+  printf ("The addition of %.2f and %.2f (%.2f+%.2f) is %.2f.\n", a, b, a, b, a+b);
+  printf ("The subtraction of %.2f and %.2f (%.2f-%.2f) is %.2f.\n", a, b, a, b, a-b);
+  printf ("The multiplication of %.2f and %.2f (%.2f*%.2f) is %.2f.\n", a, b, a, b, a*b);
+  printf ("The division of %.2f and %.2f (%.2f/%.2f) is %.2f.", a, b, a, b, a/b);
   return 0;
 }
